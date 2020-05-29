ac09092aa98ebb23bcf97d8992f87a617db60cc9
Fabiano Neumann <fabianoneumann@gmail.com>
2020-05-28 23:07:33 -0300

    removed div from PageNotFound

--- src/pages/PageNotFound.js
+++ src/pages/PageNotFound.js
@@ -6,18 +6,16 @@ import Header from '../components/Header';
 function PageNotFound(props){
     return (
         <>
-            <div>
-                <div style={ {width: '100%', background: '#0E346A'}}>
-                    <Header style={{ color: '#fff' }}/>
-                </div>
-                <div className="PageNotFound">
-                    <img src={robo} alt="Robo" />
-                    <h1>Error 404</h1>
-                    <h2>Ohh no!!!</h2>
-                    <span>Parece que a página que você estava procurando não foi encontrada.</span>
-                </div>
-                {/* <Footer /> */}
+            <div style={ {width: '100%', background: '#0E346A'}}>
+            <Header style={{ color: '#fff' }}/>
             </div>
+            <div className="PageNotFound">
+                <img src={robo} alt="Robo" />
+                <h1>Error 404</h1>
+                <h2>Ohh no!!!</h2>
+                <span>Parece que a página que você estava procurando não foi encontrada.</span>
+            </div>
+            {/* <Footer /> */}
         </>
     );
 } export default PageNotFound;
