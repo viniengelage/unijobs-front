003daf0155d48b182d79fdf747d71d7ffbce1c82
Fabiano Neumann <fabianoneumann@gmail.com>
2020-05-28 22:45:32 -0300

    adicao de comentarios

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import Header from '../components/Header';
 
 function LandingPage(props) {
-    return <h1>Hello React World! LandingPage</h1>;
+    return (
+        <>
+            <Header />
+            <h1>Hello React World! LandingPage</h1>
+        </>
+    );
 
     /** Lista Componentes React:
             <Header /> (Logo e <Menu />)
