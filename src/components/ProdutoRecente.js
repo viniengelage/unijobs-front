c0d055db27c4c3f27789bb1a97f9318f4fc2b0d6
Thiago Alves <thiago_marketingdigital@hotmail.com>
2020-06-23 19:19:03 -0300

    card adicionado recente atualizado

--- src/components/ProdutoRecente.js
+++ src/components/ProdutoRecente.js
@@ -2,14 +2,44 @@ import React from 'react';
 import './ProdutoRecente.css'
 
 function ProdutoRecente(props) {
+
+    const image = {
+        background: `url("${props.imagem}")`,
+        backgroundposition: 'center',
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'cover'
+      };
+
+    const price = () => {
+        if(props.promocao){
+            return (
+                <>
+                    R$ { props.preco }
+                    <strong> R$ { props.promocao } </strong>                    
+                </>
+            )
+        } else {
+            return (
+                <>
+                    <strong> R$ { props.preco } </strong>                    
+                </>
+            )
+        }
+    }
+
     return (
         <div className="ProdutoRecente">
-            <img src={props.imagem} alt={props.nome} />
+            <div style={image} className="img-pr"></div>
             <div className="title">
-                <strong className="categoria">{props.categoria}  <span>{props.nome}</span></strong>
+                <strong className="categoria">{props.categoria}</strong>
+                <h4>{props.nome}</h4>
                 <p className="descricao">{props.descricao}</p>
-                <strong className="publicado">Publicado por: <span>{props.usuario}</span></strong>
-                <p className="avaliacao">Estrelas: {props.avaliacao}</p>
+                <div className="info-detalhes">
+                    <a className="linkProduct" href={props.link}>+ informações</a>
+                    <p className="preco-detalhes">
+                        { price() }
+                    </p>
+                </div>
             </div>
         </div>
     );
