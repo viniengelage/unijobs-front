a39a09c32e00cf775c6a8b513662cd4afb487112
Thiago Alves <thiago_marketingdigital@hotmail.com>
2020-06-24 05:07:58 -0300

    carrossel: adicionado na section categorias

--- src/components/Categorias.js
+++ src/components/Categorias.js
@@ -1,8 +1,38 @@
 import React from 'react';
-import './Categorias.css';
+import {Carousel} from 'primereact/carousel';
 import Layout from './Layout';
 
-function Categorias() {
+import './Categorias.css';
+
+const responsiveOptions = [
+    {
+        breakpoint: '1024px',
+        numVisible: 3,
+        numScroll: 3
+    },
+    {
+        breakpoint: '768px',
+        numVisible: 2,
+        numScroll: 2
+    },
+    {
+        breakpoint: '560px',
+        numVisible: 1,
+        numScroll: 1
+    }
+];
+
+
+function Categorias({ categories }) { 
+
+    const category = (item) => {
+        return (
+            <a href="#/" className="card-category">
+                <p>{item}</p>
+            </a>
+        )
+    }
+
     return (
         <section className="Categorias">
             <Layout>
@@ -10,6 +40,16 @@ function Categorias() {
                     <h1>Categorias</h1>
                     <span>Ver todas</span>
                 </div>
+                
+                <Carousel   value={ categories }
+                            circular={ true }
+                            numScroll={ 3 }
+                            numVisible={ 5 } 
+                            itemTemplate={ category }
+                            responsiveOptions={ responsiveOptions } >
+                </Carousel>
+
+
                 {/* Criar carousel de categorias com: 
                 https://www.primefaces.org/primereact/showcase/#/carousel
                 ou
